b4fc0bbed730a7d3c3d415c6af1e20ad49a7281b
Peter Pezon <pezon@users.noreply.github.com>
2016-07-09 02:25:13 -0400

    Update zscore.sql
    experiment w/ diff. style for sql.

--- examples/sql/zscore.sql
+++ examples/sql/zscore.sql
@@ -1,29 +1,25 @@
 -- zscore.sql
 -- Perform z-score calculation
 --
-select
+select (
     t.${hiveconf:primaryid},
     t.${hiveconf:secondaryid},
-    if (
-        (100 * t.score - stats.avg)
-        / stats.var > 4,
-        4,
-        (100 * t.score - stats.avg)
-        / stats.var
-    ) zscore
-from
-    (
-        select
-            ${hiveconf:primaryid},
-            avg(score) avg,
-            stddev_pop(score) + 0.1 var
-        from
-            ${hiveconf:source_table}
-        where
-            and tscomputed = '${hiveconf:source_tscomputed}'
-        group by
-            ${hiveconf:primaryid}
-    ) stats
-    join ${hiveconf:source_table} t
-            and t.tscomputed = '${hiveconf:source_tscomputed}'
-            and t.${hiveconf:primaryid} = stats.${hiveconf:primaryid})
+        (if ((100 * t.score - stats.avg) / stats.var > 4, 4, (100 * t.score - stats.avg) / stats.var)) -- if/else; max @ 4
+    zscore
+) from (
+    select
+        ${hiveconf:primaryid},
+            avg(score)
+        avg,
+            (stddev_pop(score) + 0.1)
+        var
+    from
+        ${hiveconf:source_table}
+    where
+        tscomputed = '${hiveconf:source_tscomputed}'
+    group by
+        ${hiveconf:primaryid}
+) stats join ${hiveconf:source_table} t on (
+        t.${hiveconf:primaryid} = stats.${hiveconf:primaryid} and 
+        t.tscomputed = '${hiveconf:source_tscomputed}'
+)
